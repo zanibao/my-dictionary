67b5745365bbbc846e3e4048ec56b0aaee2564de
zanibao <65925301+zanibao@users.noreply.github.com>
2021-04-20 14:04:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        Hello!
+      </header>
+    </div>
+  );
+}
+
+export default App;
