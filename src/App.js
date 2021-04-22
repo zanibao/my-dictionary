76e6527a212ba29b2743c7a55403e5e55b1b9f07
zanibao <65925301+zanibao@users.noreply.github.com>
2021-04-22 14:07:05 +0200

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.jpg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
         Hello!
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Ana Paula Zanibão</small></footer>
+      </div>
     </div>
   );
 }
